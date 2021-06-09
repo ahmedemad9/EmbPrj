c901e0d67c26c6232ec015782eb2193bd44d80ae
Michel Gamal <68073759+MichelGamal@users.noreply.github.com>
2021-06-09 23:10:28 +0200

    Update UART_init.c

--- UART_init.c
+++ UART_init.c
@@ -1,5 +1,3 @@
-#include "tm4c123gh6pm.h"
-
 void UART_init(void)                     // called once
 {
 SYSCTL_RCGCUART_R |= SYSCTL_RCGCUART_R2; // activate UART2
@@ -9,12 +7,12 @@ GPIO_PORTD_CR_R = 0xC0;
 UART2_CTL_R = 0;                         // disable UART
 UART2_IBRD_R = 104;                      // IBRD = 16000000/(16*9600) = 104.16666
 UART2_FBRD_R = 11;                       // FBRD = (0.1667*64) + 0.5 = 11.1667
-UART2_CC_R = 0;                          //select system clock
+//UART2_CC_R = 0;                          //select system clock
 
-UART2_LCRH_R = (UART_LCRH_FEN | UART_LCRH_WLEN_8);
+UART2_LCRH_R = (UART_LCRH_FEN | UART_LCRH_WLEN_8);                       //FIFO enable 8 bit length
 UART2_CTL_R = (UART_CTL_RXE | UART_CTL_TXE | UART_CTL_UARTEN);           //with Tx
 GPIO_PORTD_AFSEL_R |= 0xC0;                                             // enable alternative function for pD6 pD7
 GPIO_PORTD_PCTL_R = (GPIO_PORTD_PCTL_R &~ 0xff000000) | (GPIO_PCTL_PD6_U2RX | GPIO_PCTL_PD7_U2TX);            //with TX
 GPIO_PORTD_AMSEL_R |= 0;                                               // analog disabled
-GPIO_PORTD_DEN_R |= 0x40;                                             // pins 6,7 set to digital
+GPIO_PORTD_DEN_R |= 0xC0;                                             // pins 6,7 set to digital
 }
