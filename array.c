e8bb6825dcf4d8e3e11a2ee1c8a642635e3a5bad
Ahmed Emad <aemad430@gmail.com>
2021-06-12 04:33:38 +0200

    mariz and ahmed edited this

---
+++ array.c
@@ -0,0 +1,16 @@
+//#include <stdio.h>
+//#include <stdlib.h>
+#include "tm4c123gh6pm.h"
+#include "tm4c123.h"
+char UART2_Receiver(void);		//I made an edit here
+
+void array (char*str, int maxlenth){
+    char c;
+    int i;
+    for(i=0; i<maxlenth; i++){
+        //c=UART2_Receiver();
+        //scanf("%c",&c);
+        //str[i]= c;
+    }
+
+}
