8c73c6ac00bf0ddf49a3fd95819cda83e61ceab9
Michel Gamal <68073759+MichelGamal@users.noreply.github.com>
2021-06-12 15:33:26 +0200

    Update and rename UART2_Receiver.c to UART2_Read.c

--- UART2_Read.c
+++ UART2_Read.c
@@ -1,7 +1,5 @@
-char UART2_Receiver(void)
+char UART2_Read(void)
 {
-char data;
 while ((UART2_FR_R & UART_FR_RXFE) != 0); // wait until Rx buffer is not full before giving it another byte
-data = UART2_DR_R;
-return (unsigned char)data;
+return UART2_DR_R & 0xFF;
 }
