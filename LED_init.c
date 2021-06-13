dea80c0fd336343dfa2c703279a76b5ce0fa2b0c
MarizSabry <85345538+MarizSabry@users.noreply.github.com>
2021-06-13 23:49:43 +0200

    Create LED_init.c

---
+++ LED_init.c
@@ -0,0 +1,18 @@
+ #include "tm4c123gh6pm.h"
+
+
+void LED_init(void)
+{
+
+SYSCTL_RCGCGPIO_R |=0x22; 	             // clock for portF
+while ( (SYSCTL_PRGPIO_R & 0x22) ==0 ) {}
+GPIO_PORTF_LOCK_R =0x4C4F434B;           // Lock
+GPIO_PORTF_CR_R |= 0x1F;                 // using only pF1 as switch
+GPIO_PORTF_DIR_R |=0x0E;                 // pF1  set to output
+GPIO_PORTF_DEN_R |=0x1F;                 // pF1 set to digital
+GPIO_PORTF_AMSEL_R &=~0x1F;              // analog disabled
+GPIO_PORTF_AFSEL_R &=~0x1F;              // disable alternative function
+GPIO_PORTF_PCTL_R = 0;
+	GPIO_PORTF_PUR_R = 0x10;
+
+}
