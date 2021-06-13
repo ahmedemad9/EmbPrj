8db4c55514b50fe49c4d47b3a76707e4dc90330a
Michel Gamal <68073759+MichelGamal@users.noreply.github.com>
2021-06-13 23:54:10 +0200

    Update array.c

--- array.c
+++ array.c
@@ -2,14 +2,14 @@
 //#include <stdlib.h>
 #include "tm4c123gh6pm.h"
 #include "tm4c123.h"
-char UART2_Receiver(void);		//I made an edit here
+char UART2_read(void);		//I made an edit here
 
 void array (char*str, int maxlenth){
     char c;
 	//int asterisk_flag=0,read_permission=0;
     int i;
     for(i=0; i<maxlenth; i++){
-        c=UART2_Receiver();
+        c=UART2_read();
 			if(c=='*') goto watermelon;
 /*			if(asterisk_flag>=1){
 				asterisk_flag++;
@@ -17,7 +17,7 @@ void array (char*str, int maxlenth){
        else str[i]= c;
     }
 		watermelon:
-		c=UART2_Receiver();
-		c=UART2_Receiver();
-		c=UART2_Receiver();
+		c=UART2_read();
+		c=UART2_read();
+		c=UART2_read();
 }
